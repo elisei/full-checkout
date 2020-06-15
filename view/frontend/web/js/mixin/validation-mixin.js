ca9512ff76e0cd6de89548ca1bcd12d285f3c402
Elisei <brunoelisei@gmail.com>
2020-06-15 10:49:01 -0300

    Fix js

--- view/frontend/web/js/mixin/validation-mixin.js
+++ view/frontend/web/js/mixin/validation-mixin.js
@@ -3,87 +3,71 @@ define([
     "Magento_Ui/js/lib/validation/utils",
 ], function($, utils) {
     "use strict";
-
+    function invalidadosComuns(value) {
+        if (value === "00000000000000" || value === "11111111111111" || value === "22222222222222" || value === "33333333333333" || value === "44444444444444" || value === "55555555555555" || value === "66666666666666" || value === "77777777777777" || value === "88888888888888" ||  value === "99999999999999" ||
+             value === "00000000000" || value === "11111111111" || value === "22222222222" || value === "33333333333" || value === "44444444444" || value === "55555555555" || value === "66666666666" || value === "77777777777" || value === "88888888888" || value === "99999999999") {
+            return true;
+        }
+        return false
+    };
     function validateCPF (value) {
-            console.log("chama a funfa");
-            let cpf = value.replace(/[^\d]+/g,"");    
-            if(cpf === "") {
-                return false;
-            }
-            if (cpf.length !== 11 || 
-                cpf === "00000000000" || 
-                cpf === "11111111111" || 
-                cpf === "22222222222" || 
-                cpf === "33333333333" || 
-                cpf === "44444444444" || 
-                cpf === "55555555555" || 
-                cpf === "66666666666" || 
-                cpf === "77777777777" || 
-                cpf === "88888888888" || 
-                cpf === "99999999999") {
-                    return false; 
-                }
-                       
-            let add = 0;
-            let i;
-            let rev; 
-            for (i=0; i < 9; i ++) {
-                add += parseInt(cpf.charAt(i)) * (10 - i);  
-            }
+        let cpf = value.replace(/[^\d]+/g,"");    
         
-                
-            rev = 11 - (add % 11);  
-            if (rev === 10 || rev === 11)     
-                rev = 0;    
-            if (rev !== parseInt(cpf.charAt(9))) {
-                return false; 
-            }
-                        
-            // Valida 2o digito 
-            add = 0;    
-            for (i = 0; i < 10; i ++){
-                add += parseInt(cpf.charAt(i)) * (11 - i);
-            }
-        
-                
-            rev = 11 - (add % 11); 
+        if (cpf.length !== 11) {
+            return false; 
+        }
 
-            if (rev === 10 || rev === 11) {
-                rev = 0;
-            }
-                
-            if (rev !== parseInt(cpf.charAt(10))) {
-                return false;
-            }
-                  
-            return true;  
-        };
-        function validateCNPJ(value) {
-            let CNPJ = value.replace(/[^\d]+/g,"");
-            if(CNPJ === "") {
+        if(invalidadosComuns(cpf)) {
+            return false;
+        }
+                   
+        let add = 0;
+        let i;
+        let rev; 
+        for (i=0; i < 9; i ++) {
+            add += parseInt(cpf.charAt(i)) * (10 - i);  
+        }
+    
+            
+        rev = 11 - (add % 11);  
+        if (rev === 10 || rev === 11) {
+            rev = 0;    
+        }
+        if (rev !== parseInt(cpf.charAt(9))) {
+            return false; 
+        }
+         
+        add = 0;    
+        for (i = 0; i < 10; i ++){
+            add += parseInt(cpf.charAt(i)) * (11 - i);
+        }
+    
+        rev = 11 - (add % 11); 
+
+        if (rev === 10 || rev === 11) {
+            rev = 0;
+        }
+            
+        if (rev !== parseInt(cpf.charAt(10))) {
+            return false;
+        }
+              
+        return true;  
+    };
+    function validateCNPJ(value) {
+         let cnpj = value.replace(/[^\d]+/g,'');
+            
+            if (cnpj.length !== 14) {
                 return false;
             }
 
-            if (CNPJ.length !== 14) {
+            if(invalidadosComuns(cnpj)) {
                 return false;
             }
-                
-            if (CNPJ === "00000000000000" || 
-                CNPJ === "11111111111111" || 
-                CNPJ === "22222222222222" || 
-                CNPJ === "33333333333333" || 
-                CNPJ === "44444444444444" || 
-                CNPJ === "55555555555555" || 
-                CNPJ === "66666666666666" || 
-                CNPJ === "77777777777777" || 
-                CNPJ === "88888888888888" || 
-                CNPJ === "99999999999999") {
-                return false;
-            }
-                
-            let tamanho = CNPJ.length - 2
-            let numeros = CNPJ.substring(0,tamanho);
-            let digitos = CNPJ.substring(tamanho);
+            
+            let tamanho = cnpj.length - 2
+            let numeros = cnpj.substring(0,tamanho);
+            let digitos = cnpj.substring(tamanho);
             let soma = 0;
             let pos = tamanho - 7;
             let i;
@@ -91,33 +75,34 @@ define([
             for (i = tamanho; i >= 1; i--) {
               soma += numeros.charAt(tamanho - i) * pos--;
               if (pos < 2) {
-                  pos = 9;
+                    pos = 9;
               }
-                  
             }
             resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-            if (resultado !== digitos.charAt(0)) {
+            if (resultado != digitos.charAt(0)) {
                 return false;
             }
-               
                  
             tamanho = tamanho + 1;
-            numeros = CNPJ.substring(0,tamanho);
+            numeros = cnpj.substring(0,tamanho);
             soma = 0;
             pos = tamanho - 7;
             for (i = tamanho; i >= 1; i--) {
               soma += numeros.charAt(tamanho - i) * pos--;
-              if (pos < 2) {
-                pos = 9;
+              if (pos < 2){
+                    pos = 9;
               }
             }
             resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-            if (resultado !== digitos.charAt(1)) {
-                return false;
-            }
+            
+            digitos = parseInt(digitos.charAt(1));
 
+            if (resultado !== digitos){
+               return false; 
+            }
+                  
             return true;
-        };
+    };
      
     return function(validator) {
 
