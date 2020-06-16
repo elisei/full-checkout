4db59cf78436860b5400739e033d803a4891c12f
Elisei <brunoelisei@gmail.com>
2020-06-16 12:51:28 -0300

    Fix jsLint

--- view/frontend/web/js/mixin/validation-mixin.js
+++ view/frontend/web/js/mixin/validation-mixin.js
@@ -9,14 +9,14 @@ define([
             if (value === "00000000000000" || value === "11111111111111" || value === "22222222222222" || value === "33333333333333" || value === "44444444444444" || value === "55555555555555" || value === "66666666666666" || value === "77777777777777" || value === "88888888888888" || value === "99999999999999") {
                 return true;
             }
-            return false
+            return false;
         };
 
         var invalidosComunsCPF = function(value) {
             if (value === "00000000000" || value === "11111111111" || value === "22222222222" || value === "33333333333" || value === "44444444444" || value === "55555555555" || value === "66666666666" || value === "77777777777" || value === "88888888888" || value === "99999999999") {
                 return true;
             }
-            return false
+            return false;
         };
 
         var validateCPF = function(value) {
