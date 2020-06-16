907e69d1045bc407931644e9bd1c21ae2d3268e8
Elisei <brunoelisei@gmail.com>
2020-06-16 12:48:56 -0300

    Fix jsLint

--- view/frontend/web/js/mixin/validation-mixin.js
+++ view/frontend/web/js/mixin/validation-mixin.js
@@ -4,153 +4,115 @@ define([
 ], function($, utils) {
     "use strict";
 
-    function invalidosComunsCNPJ(value) {
-        if (value === "00000000000000" || value === "11111111111111" || value === "22222222222222" || value === "33333333333333" || value === "44444444444444" || value === "55555555555555" || value === "66666666666666" || value === "77777777777777" || value === "88888888888888" || value === "99999999999999") {
-            return true;
-        }
-        return false
-    }
-
-    function invalidosComunsCPF(value) {
-        if (value === "00000000000" || value === "11111111111" || value === "22222222222" || value === "33333333333" || value === "44444444444" || value === "55555555555" || value === "66666666666" || value === "77777777777" || value === "88888888888" || value === "99999999999") {
-            return true;
-        }
-        return false
-    }
-
-    function validateCPF(val) {
-        let cpf = val.replace(/[^\d]+/g, "");
-
-        let v1 = 0;
-        let v2 = 0;
-        let aux = false;
-
-        if (cpf.length !== 11) {
-            return false;
-        }
-
-        if (invalidosComunsCPF(cpf)) {
-            return false;
-        }
-
-        for (let i = 1; cpf.length > i; i++) {
-            if (cpf[i - 1] != cpf[i]) {
-                aux = true;
-            }
-        }
-
-        if (aux === false) {
-            return false;
-        }
-
-        for (let k = 0, p = 10;
-            (cpf.length - 2) > k; k++, p--) {
-            v1 += cpf[k] * p;
-        }
-
-        v1 = ((v1 * 10) % 11);
-
-        if (v1 === 10) {
-            v1 = 0;
-        }
-
-        if (v1 !== cpf[9]) {
-            return false;
-        }
-
-        for (let j = 0, p = 11;
-            (cpf.length - 1) > j; j++, p--) {
-            v2 += cpf[j] * p;
-        }
-
-        v2 = ((v2 * 10) % 11);
-
-        if (v2 === 10) {
-            v2 = 0;
-        }
-
-        if (v2 != cpf[10]) {
-            return false;
-        } else {
-            return true;
-        }
-
-        return true;
-    }
-
-    function validateCNPJ(value) {
-        let cnpj = value.replace(/[^\d]+/g, "");
-
-        if (cnpj.length !== 14) {
-            return false;
-        }
-
-        if (invalidosComunsCNPJ(cnpj)) {
-            return false;
-        }
-
-        let v1 = 0;
-        let v2 = 0;
-        let aux = false;
-
-        for (let i = 1; cnpj.length > i; i++) {
-            if (cnpj[i - 1] !== cnpj[i]) {
-                aux = true;
-            }
-        }
-
-        if (aux === false) {
-            return false;
-        }
-
-        for (let k = 0, p1 = 5, p2 = 13;
-            (cnpj.length - 2) > k; k++, p1--, p2--) {
-            if (p1 >= 2) {
-                v1 += cnpj[k] * p1;
-            } else {
-                v1 += cnpj[k] * p2;
-            }
-        }
-
-        v1 = (v1 % 11);
-
-        if (v1 < 2) {
-            v1 = 0;
-        } else {
-            v1 = (11 - v1);
-        }
-
-        if (v1 != cnpj[12]) {
-            return false;
-        }
-
-        for (let j = 0, p1 = 6, p2 = 14;
-            (cnpj.length - 1) > j; j++, p1--, p2--) {
-            if (p1 >= 2) {
-                v2 += cnpj[j] * p1;
-            } else {
-                v2 += cnpj[j] * p2;
-            }
-        }
-
-        v2 = (v2 % 11);
-
-        if (v2 < 2) {
-            v2 = 0;
-        } else {
-            v2 = (11 - v2);
-        }
-
-        if (v2 !== cnpj[13]) {
-            return false;
-        } else {
-            return true;
-        }
-
-        return true;
-    }
-
     return function(validator) {
+        var invalidosComunsCNPJ = function(value) {
+            if (value === "00000000000000" || value === "11111111111111" || value === "22222222222222" || value === "33333333333333" || value === "44444444444444" || value === "55555555555555" || value === "66666666666666" || value === "77777777777777" || value === "88888888888888" || value === "99999999999999") {
+                return true;
+            }
+            return false
+        };
+
+        var invalidosComunsCPF = function(value) {
+            if (value === "00000000000" || value === "11111111111" || value === "22222222222" || value === "33333333333" || value === "44444444444" || value === "55555555555" || value === "66666666666" || value === "77777777777" || value === "88888888888" || value === "99999999999") {
+                return true;
+            }
+            return false
+        };
+
+        var validateCPF = function(value) {
+            let cpf = value.replace(/[^\d]+/g, "");
+
+            if (cpf.length !== 11) {
+                return false;
+            }
+
+            if (invalidosComunsCPF(cpf)) {
+                return false;
+            }
+
+            let add = 0;
+            let i;
+            let j;
+            let rev;
+            for (i = 0; i < 9; i++) {
+                add += parseInt(cpf.charAt(i), 10) * (10 - i);
+            }
+
+            rev = 11 - (add % 11);
+            if (rev === 10 || rev === 11) {
+                rev = 0;
+            }
+            if (rev !== parseInt(cpf.charAt(9), 10)) {
+                return false;
+            }
+
+            add = 0;
+            for (j = 0; j < 10; j++) {
+                add += parseInt(cpf.charAt(j), 10) * (11 - j);
+            }
+
+            rev = 11 - (add % 11);
+
+            if (rev === 10 || rev === 11) {
+                rev = 0;
+            }
+
+            if (rev !== parseInt(cpf.charAt(10), 10)) {
+                return false;
+            }
+
+            return true;
+        };
+
+        var validateCNPJ = function(value) {
+            let cnpj = value.replace(/[^\d]+/g, "");
+
+            if (cnpj.length !== 14) {
+                return false;
+            }
+
+            if (invalidosComunsCNPJ(cnpj)) {
+                return false;
+            }
+
+            let tamanho = cnpj.length - 2
+            let numeros = cnpj.substring(0, tamanho);
+            let digitos = cnpj.substring(tamanho);
+            let soma = 0;
+            let pos = tamanho - 7;
+            let i;
+            let j;
+            let resultado;
+            for (i = tamanho; i >= 1; i--) {
+                soma += numeros.charAt(tamanho - i) * pos--;
+                if (pos < 2) {
+                    pos = 9;
+                }
+            }
+            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+
+            if (resultado !== parseInt(digitos.charAt(0), 10)) {
+                return false;
+            }
+
+            tamanho = tamanho + 1;
+            numeros = cnpj.substring(0, tamanho);
+            soma = 0;
+            pos = tamanho - 7;
+            for (j = tamanho; j >= 1; j--) {
+                soma += numeros.charAt(tamanho - j) * pos--;
+                if (pos < 2) {
+                    pos = 9;
+                }
+            }
+            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+
+            if (resultado !== parseInt(digitos.charAt(1), 10)) {
+                return false;
+            }
+
+            return true;
+        };
 
         validator.addRule(
             "telephone-br-rule",
@@ -163,6 +125,7 @@ define([
             },
             $.mage.__("Insira um telefone válido, pode ser fixo ou celular.")
         );
+
         validator.addRule(
             "vatid-br-rule",
             function(value) {
@@ -176,7 +139,6 @@ define([
             $.mage.__("Insira um documento fiscal válido, pode ser CPF ou CNPJ")
         );
 
-
         return validator;
-    }
+    };
 });
